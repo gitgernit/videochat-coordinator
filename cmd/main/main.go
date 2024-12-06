86bcf6ab64b70f2bd17dca9826570e4a5d0c387f
doas root <petr.zakharov.anton@gmail.com>
2024-12-06 21:46:48 +0300

    feat(cmd): setup main() entrypoint function

---
+++ cmd/main/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"gitlab.crja72.ru/gospec/go5/rooms/pkg/logger"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	serviceName = "coordinator"
+)
+
+func main() {
+	ctx := context.Background()
+	mainLogger := logger.New(serviceName)
+	ctx = context.WithValue(ctx, logger.LoggerKey, mainLogger)
+
+	//cfg, err := config.New()
+	//if err != nil {
+	//	mainLogger.Error(ctx, err.Error())
+	//	return
+	//}
+
+	graceCh := make(chan os.Signal, 1)
+	signal.Notify(graceCh, syscall.SIGINT, syscall.SIGTERM)
+
+	mainLogger.Info(ctx, "Successfully started")
+
+	<-graceCh
+
+	mainLogger.Info(ctx, "Successfully shut down")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gitlab.crja72.ru/gospec/go5/coordinator
 
 go 1.23.2
+
+require gitlab.crja72.ru/gospec/go5/rooms v0.0.0-20241206183921-eb4332686f86
+
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
